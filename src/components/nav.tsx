8d61c94e6c22e7bca68728bb5550d9256c4fc698
na0nie <naonchun@gmail.com>
2024-12-03 15:21:46 -0800

    mobile nav title

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -161,7 +161,7 @@ function Nav() {
           <Disclosure.Panel className="md:hidden">
             <div className="space-y-1 px-2 pt-0 pb-3">
               <p className="text-[#41ffca] font-medium text-base pt-0 pb-1 px-3 block">
-                Eviction Notices Nov 2023 - Dec 2024
+                Eviction Notices Feb 2023 - Nov 2024
               </p>
               {navarraycurrent().map((item: newiteminterface) => (
                 <Disclosure.Button
